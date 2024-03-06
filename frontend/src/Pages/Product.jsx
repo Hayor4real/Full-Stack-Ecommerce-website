a516568f7e1bea2d3978e15573f89eb62cb4d4ea
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2024-03-06 18:49:53 +0100

    code added to find a particular id

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import Breadcrums from "../Components/Breadcrums/Breadcrum";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
+//import DescriptionBox from "../Components/DescriptionBox/DescriptionBox";
+//import RelatedProducts from "../Components/RelatedProducts/RelatedProducts";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../Context/ShopContext";
 
 const Product = () => {
-  return <div></div>;
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
+  return (
+    <div>
+      <Breadcrums product={product} />
+      <ProductDisplay product={product} />
+    </div>
+  );
 };
 
 export default Product;
