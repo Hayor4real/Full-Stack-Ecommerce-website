5ddf5dc3b4c37905852470292a1d29a0e68958da
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2024-03-10 21:14:30 +0100

    images imported and added some datas

--- frontend/src/Components/Assets/data.js
+++ frontend/src/Components/Assets/data.js
@@ -1,33 +1,52 @@
-import p1_img from './product_1.png'
-import p2_img from './product_2.png'
-import p3_img from './product_3.png'
-import p4_img from './product_4.png'
+import p1_img from "./product_1.png";
+import p2_img from "./product_7.png";
+import p3_img from "./Bb.jpg";
+import p4_img from "./cc.jpg";
+import p5_img from "./product_5.png";
+import p6_img from "./product_6.png";
 
 let data_product = [
   {
-    id:1,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-    image:p1_img,
-    new_price:50.00,
-    old_price:80.50,
+    id: 1,
+    name: "Elegant Black jacket  Stylish Women",
+    image: p1_img,
+    new_price: 59.99,
+    old_price: 95.5,
   },
-  {id:2,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-    image:p2_img,
-    new_price:85.00,
-    old_price:120.50,
+  {
+    id: 2,
+    name: "White Blouse",
+    image: p2_img,
+    new_price: 85.0,
+    old_price: 120.5,
   },
-  {id:3,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-    image:p3_img,
-    new_price:60.00,
-    old_price:100.50,
+  {
+    id: 3,
+    name: "black gawn ",
+    image: p3_img,
+    new_price: 60.0,
+    old_price: 100.5,
   },
-  {id:4,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-    image:p4_img,
-    new_price:100.00,
-    old_price:150.00,
+  {
+    id: 4,
+    name: "Trendy Women's Peplum Hem Blouse",
+    image: p4_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+  {
+    id: 5,
+    name: "Fashionable Women's Striped Blouse",
+    image: p5_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+  {
+    id: 6,
+    name: "Stylish Women's Peplum Hem Blouse",
+    image: p6_img,
+    new_price: 79.5,
+    old_price: 115,
   },
 ];
 
