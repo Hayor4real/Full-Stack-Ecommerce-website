86dcd26af56f66a8c18f5667c2bae0e68038f7fd
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2024-03-10 21:13:20 +0100

    image imported

--- frontend/src/Components/Assets/all_product.js
+++ frontend/src/Components/Assets/all_product.js
@@ -1,20 +1,20 @@
 import p1_img from "./product_1.png";
-import p2_img from "./product_2.png";
-import p3_img from "./product_3.png";
-import p4_img from "./product_4.png";
+import p2_img from "./product_7.png";
+import p3_img from "./Bb.jpg";
+import p4_img from "./cc.jpg";
 import p5_img from "./product_5.png";
 import p6_img from "./product_6.png";
-import p7_img from "./product_7.png";
-import p8_img from "./product_8.png";
+import p7_img from "./cc.jpg";
+import p8_img from "./f4.jpg";
 import p9_img from "./product_9.png";
 import p10_img from "./product_10.png";
 import p11_img from "./product_11.png";
-import p12_img from "./product_12.png";
+import p12_img from "./f1.jpg";
 import p13_img from "./product_13.png";
-import p14_img from "./product_14.png";
-import p15_img from "./product_15.png";
+import p14_img from "./f3.jpg";
+import p15_img from "./f5.jpg";
 import p16_img from "./product_16.png";
-import p17_img from "./product_17.png";
+import p17_img from "./f7.jpg";
 import p18_img from "./product_18.png";
 import p19_img from "./product_19.png";
 import p20_img from "./product_20.png";
@@ -25,36 +25,47 @@ import p24_img from "./product_24.png";
 import p25_img from "./product_25.png";
 import p26_img from "./product_26.png";
 import p27_img from "./product_27.png";
-import p28_img from "./product_28.png";
+import p28_img from "./f8.jpg";
 import p29_img from "./product_29.png";
 import p30_img from "./product_30.png";
 import p31_img from "./product_31.png";
 import p32_img from "./product_32.png";
 import p33_img from "./product_33.png";
 import p34_img from "./product_34.png";
-import p35_img from "./product_35.png";
+import p35_img from "./f2.jpg";
 import p36_img from "./product_36.png";
+import p37_img from "./n1.jpg";
+import p38_img from "./n2.jpg";
+import p39_img from "./n3.jpg";
+import p40_img from "./n4.jpg";
+import p41_img from "./n5.jpg";
+import p42_img from "./n6.jpg";
+import p43_img from "./n7.jpg";
+import p44_img from "./p1.jpg";
+import p45_img from "./p2.jpg";
+import p46_img from "./p3.jpg";
+import p47_img from "./p4.jpg";
 
 let all_product = [
   {
     id: 1,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Elegant Black jacket  Stylish Women",
     category: "women",
     image: p1_img,
-    new_price: 50.0,
-    old_price: 80.5,
+    new_price: 59.99,
+    old_price: 95.5,
   },
   {
     id: 2,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "White blouse",
     category: "women",
     image: p2_img,
-    new_price: 85.0,
+    new_price: 85,
     old_price: 120.5,
   },
   {
     id: 3,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "black gawn",
     category: "women",
     image: p3_img,
     new_price: 60.0,
@@ -62,267 +73,355 @@ let all_product = [
   },
   {
     id: 4,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Trendy Women's Peplum Hem Blouse",
     category: "women",
     image: p4_img,
-    new_price: 100.0,
-    old_price: 150.0,
+    new_price: 100,
+    old_price: 150,
   },
   {
     id: 5,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Fashionable Women's Striped Blouse",
     category: "women",
     image: p5_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 100,
+    old_price: 150,
   },
   {
     id: 6,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Stylish Women's Peplum Hem Blouse",
     category: "women",
     image: p6_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 79.5,
+    old_price: 115.0,
   },
   {
     id: 7,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Trendsetting Women's Peplum Hem Blouse",
     category: "women",
     image: p7_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 70.0,
+    old_price: 105.0,
   },
   {
     id: 8,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-    category: "women",
+    name: "Classy Men's Casual Shirt",
+    category: "men",
     image: p8_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 95.0,
+    old_price: 130.0,
   },
   {
     id: 9,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Charming Women's Peplum Hem Blouse",
     category: "women",
     image: p9_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 75.0,
+    old_price: 110.0,
   },
   {
     id: 10,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Trendy Women's Peplum Hem Blouse",
     category: "women",
     image: p10_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 82.5,
+    old_price: 118.0,
   },
   {
     id: 11,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Elegant Women's Peplum Hem Blouse",
     category: "women",
     image: p11_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 88.0,
+    old_price: 125.5,
   },
   {
     id: 12,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-    category: "women",
+    name: "Classic Men's Casual Shirt",
+    category: "men",
     image: p12_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 96.5,
+    old_price: 135.0,
   },
   {
     id: 13,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men's Green Slim Fit Bomber Jacket",
     category: "men",
     image: p13_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 89.5,
+    old_price: 125.5,
   },
   {
     id: 14,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men Orange Colour shirt",
     category: "men",
     image: p14_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 99.99,
+    old_price: 145.0,
   },
   {
     id: 15,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men's Flower design shirt",
     category: "men",
     image: p15_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 92.0,
+    old_price: 130.5,
   },
   {
     id: 16,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men's Red Colour Bomber Jacket",
     category: "men",
     image: p16_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 80.0,
+    old_price: 115.5,
   },
   {
     id: 17,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
-    category: "men",
+    name: "Women fashion blouse",
+    category: "women",
     image: p17_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 74.99,
+    old_price: 105.0,
   },
   {
     id: 18,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men's  Slim Fit white Bomber Jacket",
     category: "men",
     image: p18_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 94.99,
+    old_price: 135.0,
   },
   {
     id: 19,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men's white hoodie",
     category: "men",
     image: p19_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 87.5,
+    old_price: 120.0,
   },
   {
     id: 20,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men's Blue Jacket",
     category: "men",
     image: p20_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 76.5,
+    old_price: 112.0,
   },
   {
     id: 21,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men's Multi colour Jacket",
     category: "men",
     image: p21_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 78.0,
+    old_price: 118.5,
   },
   {
     id: 22,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men's white Slim Fit hoodie Jacket",
     category: "men",
     image: p22_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 82.5,
+    old_price: 116.0,
   },
   {
     id: 23,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men's Bomber Jacket",
     category: "men",
     image: p23_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 79.99,
+    old_price: 119.0,
   },
   {
     id: 24,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men's Black Jacket",
     category: "men",
     image: p24_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 84.5,
+    old_price: 121.5,
   },
   {
     id: 25,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Boy's long sleeve Jacket",
     category: "kid",
     image: p25_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 65.0,
+    old_price: 95.0,
   },
   {
     id: 26,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Boy's black jacket",
     category: "kid",
     image: p26_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 72.0,
+    old_price: 105.0,
   },
   {
     id: 27,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Boys Multi colour jacket",
     category: "kid",
     image: p27_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 68.5,
+    old_price: 98.0,
   },
   {
     id: 28,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Boys Navy Blue shirt",
     category: "kid",
     image: p28_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 74.99,
+    old_price: 110.0,
   },
   {
     id: 29,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Boys Hooded Sweatshirt",
     category: "kid",
     image: p29_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 79.5,
+    old_price: 112.5,
   },
   {
     id: 30,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "children Winter Jacket",
     category: "kid",
     image: p30_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 68.0,
+    old_price: 100.0,
   },
   {
     id: 31,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Jean Top",
     category: "kid",
     image: p31_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 71.5,
+    old_price: 104.5,
   },
   {
     id: 32,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Blue top for kids",
     category: "kid",
     image: p32_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 75.0,
+    old_price: 108.0,
   },
   {
     id: 33,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Blue long sleeve",
     category: "kid",
     image: p33_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 69.99,
+    old_price: 102.0,
   },
   {
     id: 34,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Blue Hooded Sweatshirt",
     category: "kid",
     image: p34_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 77.0,
+    old_price: 110.5,
   },
   {
     id: 35,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Boys Colour shirt",
     category: "kid",
     image: p35_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 73.5,
+    old_price: 105.5,
   },
   {
     id: 36,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Designer Jacket shirt and trouser ",
     category: "kid",
     image: p36_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 72.0,
+    old_price: 100.0,
+  },
+  {
+    id: 37,
+    name: "Long sleeve shirt ",
+    category: "men",
+    image: p37_img,
+    new_price: 76.5,
+    old_price: 110.0,
+  },
+  {
+    id: 38,
+    name: "White Long Sleeve",
+    category: "men",
+    image: p38_img,
+    new_price: 68.5,
+    old_price: 105.0,
+  },
+  {
+    id: 39,
+    name: " Men Ash colour shirt",
+    category: "men",
+    image: p39_img,
+    new_price: 70.0,
+    old_price: 108.0,
+  },
+  {
+    id: 40,
+    name: "Jean top",
+    category: "men",
+    image: p40_img,
+    new_price: 74.5,
+    old_price: 112.5,
+  },
+  {
+    id: 41,
+    name: "Shorts for Men",
+    category: "men",
+    image: p41_img,
+    new_price: 69.99,
+    old_price: 104.0,
+  },
+  {
+    id: 42,
+    name: "Yellow short sleeve",
+    category: "men",
+    image: p42_img,
+    new_price: 77.0,
+    old_price: 115.0,
+  },
+  {
+    id: 43,
+    name: "Black short Sleeve",
+    category: "men",
+    image: p43_img,
+    new_price: 71.5,
+    old_price: 108.5,
+  },
+  {
+    id: 44,
+    name: "Men long sleeve shirt",
+    category: "men",
+    image: p44_img,
+    new_price: 79.5,
+    old_price: 120.0,
+  },
+  {
+    id: 45,
+    name: "Long sleeve shirt for Men",
+    category: "men",
+    image: p45_img,
+    new_price: 74.0,
+    old_price: 114.5,
+  },
+  {
+    id: 46,
+    name: "chinos for men",
+    category: "men",
+    image: p46_img,
+    new_price: 94.99,
+    old_price: 135.99,
+  },
+  {
+    id: 47,
+    name: "Men's pant trouser",
+    category: "men",
+    image: p47_img,
+    new_price: 79.99,
+    old_price: 140.99,
   },
 ];
 
