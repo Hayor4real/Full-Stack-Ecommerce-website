bf8b4bd897dbc25227996bc380a736425844e635
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2024-03-14 09:57:46 +0100

    imported some components

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,23 +1,20 @@
 import React, { useContext } from "react";
 import Breadcrums from "../Components/Breadcrums/Breadcrum";
 import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
-//import DescriptionBox from "../Components/DescriptionBox/DescriptionBox";
-//import RelatedProducts from "../Components/RelatedProducts/RelatedProducts";
+import DescriptionBox from "../Components/DescriptionBox/DescriptionBox";
+
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../Context/ShopContext";
-import DescriptionBox from "../Components/DescriptionBox/DescriptionBox";
-import RelatedProducts from "../Components/RelatedProducts/RelatedProducts";
 
 const Product = () => {
-  const { all_product } = useContext(ShopContext);
+  const { products } = useContext(ShopContext);
   const { productId } = useParams();
-  const product = all_product.find((e) => e.id === Number(productId));
+  const product = products.find((e) => e.id === Number(productId));
   return (
     <div>
       <Breadcrums product={product} />
       <ProductDisplay product={product} />
       <DescriptionBox />
-      <RelatedProducts />
     </div>
   );
 };
