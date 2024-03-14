17f3302b3a64695cb7c851fb6ba6d34fc409b5c8
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2024-03-14 10:00:06 +0100

    images added

---
+++ admin/src/assets/new_collections.js
@@ -0,0 +1,63 @@
+import p1_img from "./f1.jpg";
+import p2_img from "./f2.jpg";
+import p3_img from "./f3.jpg";
+import p4_img from "./f4.jpg";
+import p5_img from "./f5.jpg";
+//import p6_img from "./f6.jpg";
+import p7_img from "./f7.jpg";
+import p8_img from "./f8.jpg";
+
+let new_collections = [
+  {
+    id: 12,
+    name: "Classic Men's Casual Shirt",
+    image: p1_img,
+    new_price: 96.5,
+    old_price: 135,
+  },
+  {
+    id: 35,
+    name: "Boys Colour shirt",
+    image: p2_img,
+    new_price: 73.5,
+    old_price: 105.5,
+  },
+  {
+    id: 14,
+    name: "Men Orange Colour shirt",
+    image: p3_img,
+    new_price: 99.99,
+    old_price: 145.0,
+  },
+  {
+    id: 8,
+    name: "Classy Men's Casual Shirt",
+    image: p4_img,
+    new_price: 95.0,
+    old_price: 130.0,
+  },
+  {
+    id: 15,
+    name: "Men's Flower design shirt",
+    image: p5_img,
+    new_price: 92.0,
+    old_price: 130.5,
+  },
+
+  {
+    id: 17,
+    name: "Women fashion blouse",
+    image: p7_img,
+    new_price: 74.99,
+    old_price: 105.0,
+  },
+  {
+    id: 28,
+    name: "Boys Navy Blue shirt",
+    image: p8_img,
+    new_price: 74.99,
+    old_price: 110.0,
+  },
+];
+
+export default new_collections;
