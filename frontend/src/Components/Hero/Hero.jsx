61f8e5d73320eb9351913c6b99f44dd3c300fbbf
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2024-03-25 19:20:19 +0100

    fixed the conflicts

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -2,38 +2,6 @@ import React from "react";
 import "./Hero.css";
 import hand_icon from "../Assets/fashion.jpg";
 //import arrow_icon from "../Assets/arrow.png";
-//import hero_image from "../Assets/herobanner.jpg";
-import { Link } from "react-router-dom";
-const Hero = () => {
-  return (
-    <div className="hero">
-      <div className="hero-left">
-        <h2>NEW ARRIVALS ONLY</h2>
-        <div>
-          <div className="hero-hand-icon">
-            <p>new</p>
-            <img src={hand_icon} alt="" />
-          </div>
-          <p>collections</p>
-          <p>for everyone</p>
-        </div>
-        <div className="hero-latest-btn">
-          <Link style={{ textDecoration: "none" }} to="/login">
-            <button>Check our products</button>
-          </Link>
-        </div>
-      </div>
-      <div className="hero-right"></div>
-    </div>
-  );
-};
-
-export default Hero;
-
-import React from "react";
-import "./Hero.css";
-import hand_icon from "../Assets/fashion.jpg";
-//import arrow_icon from "../Assets/arrow.png";
 import hero_image from "../Assets/herobanner.jpg";
 import { Link } from "react-router-dom";
 const Hero = () => {
@@ -55,6 +23,7 @@ const Hero = () => {
           </Link>
         </div>
       </div>
+
       <div className="hero-right">
         <img src={hero_image} alt="" />
       </div>
